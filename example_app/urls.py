a70ad249d86cc521debcf1f309e8cfc95e18c79f
TehGaa <syosuasaputra1010@gmail.com>
2023-05-15 13:22:39 +0700

    Adding database

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from example_app.views import index
+from example_app.views import index, ex_query
 
 app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('ex_query', ex_query, name='ex_query')
 ]
