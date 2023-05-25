58996f9bedddde1e8cdb55b3d53f9be3952dc1e5
Trias Ahmad Fairuz <trias3011@gmail.com>
2023-05-25 21:48:53 +0700

    dashboard udah, krem udah

--- krem/urls.py
+++ krem/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('pembelian_tiket2', pembelian_tiket2, name="pembelian_tiket2"),
     path('pembelian_tiket3', pembelian_tiket3, name="pembelian_tiket3"),
     path('pembelian_tiket4', pembelian_tiket4, name="pembelian_tiket4"),
+    path('list_pertandingan_penonton', list_pertandingan_penonton, name="list_pertandingan_penonton"),
+    path('list_pertandingan_manajer', list_pertandingan_manajer, name="list_pertandingan_manajer"),
+    path('history_rapat', history_rapat, name="history_rapat"),
+    path('isi_rapat', isi_rapat, name="isi_rapat")
 ]
--- krem/views.py
+++ krem/views.py
@@ -73,7 +73,9 @@ def pembelian_tiket3(request):
 
         response = HttpResponseRedirect(reverse('krem:pembelian_tiket3'))
         response['location'] = reverse('krem:pembelian_tiket3')
+
         response.set_cookie("id_pertandingan", id_pertandingan)
+
         print(response)
         return response
     if (request.method == "GET"):
@@ -88,12 +90,10 @@ def pembelian_tiket4(request):
 
         id_penonton = request.COOKIES.get('id_role')
         id_pertandingan = request.COOKIES.get('id_pertandingan')
+
         jenis_tiket = request.POST.get('jenis_tiket')
         jenis_pembayaran = request.POST.get('jenis_pembayaran')
         nomor_receipt = generate_random_receipt()
-
-        string = f"'{id_penonton}', '{id_pertandingan}', '{jenis_tiket}', '{jenis_pembayaran}', '{nomor_receipt}'"
-        print(string)
 
         query = f"INSERT INTO PEMBELIAN_TIKET VALUES('{nomor_receipt}', '{id_penonton}', '{jenis_tiket}', '{jenis_pembayaran}', '{id_pertandingan}')"
         cur.execute(query)
@@ -104,6 +104,108 @@ def pembelian_tiket4(request):
         return response
 
     return 
+
+def list_pertandingan_penonton(request):
+
+    query=f"""
+            select start_datetime, end_datetime, STRING_AGG(nama_tim, ' vs ') as tim_bertanding, s.nama 
+            from tim_pertandingan as tp 
+            inner join pertandingan as p on tp.id_pertandingan=p.id_pertandingan -- add a dot here
+            inner join stadium as s on p.stadium=s.id_stadium -- change stadium to s
+            group by s.id_stadium, p.id_pertandingan
+            order by start_datetime
+            """
+    cur.execute(query)
+    list_pertandingan = cur.fetchall()
+    context = {'pertandingans' : list_pertandingan}
+
+    return render(request, 'list_pertandingan_penonton.html', context)
+
+
+def list_pertandingan_manajer(request):
+    id_manajer = request.COOKIES.get('id_role')
+
+    query=f"""select p.id_pertandingan, start_datetime, end_datetime, STRING_AGG(nama_tim, ' vs ') as tim_bertanding, s.nama 
+            from tim_pertandingan as tp 
+            inner join pertandingan as p on tp.id_pertandingan=p.id_pertandingan -- add a dot here
+            inner join stadium as s on p.stadium=s.id_stadium -- change stadium to s
+		    where (p.id_pertandingan) in 
+            (select id_pertandingan 
+            from tim_manajer natural 
+            join tim_pertandingan where id_manajer='{id_manajer}')
+            group by s.id_stadium, p.id_pertandingan
+            order by start_datetime
+            """
+    cur.execute(query)
+    list_pertandingan = cur.fetchall()
+    context = {'pertandingans' : list_pertandingan}
+
+    return render(request, 'list_pertandingan_manajer.html', context)
+
+def history_rapat(request):
+    id_manajer = request.COOKIES.get('id_role')
+
+    query = f"""
+    select array_to_string(array_agg("nama_tim"),' VS ') as rapat_tim, panitia.username as nama_panitia, stadium.nama as nama_stadium, pertandingan.start_datetime as tanggal_dan_waktu, rapat.id_pertandingan
+    from rapat, panitia, tim_pertandingan, stadium, pertandingan
+    where tim_pertandingan.id_pertandingan = rapat.id_pertandingan and pertandingan.id_pertandingan = rapat.id_pertandingan and pertandingan.stadium = stadium.id_stadium and rapat.perwakilan_panitia = panitia.id_panitia
+    AND (manajer_tim_a='{id_manajer}' or manajer_tim_b='{id_manajer}')
+    group by pertandingan.start_datetime, stadium.nama, panitia.username, rapat.id_pertandingan
+    order by pertandingan.start_datetime
+    """
+    cur.execute(query)
+    rapats = cur.fetchall()
+
+    context = {
+        'rapats':rapats
+    }
+    return render(request, 'history_rapat.html', context)
+
+def isi_rapat(request):
+    if (request.method == "POST"):
+
+        data = json.loads(request.body)
+        id_pertandingan = data.get('id_pertandingan')
+        print(id_pertandingan + " ini id")
+
+        response = HttpResponseRedirect(reverse('krem:isi_rapat'))
+        response['location'] = reverse('krem:isi_rapat')
+
+        response.set_cookie("id_pertandingan", id_pertandingan)
+
+        print(response)
+        return response
+
+
+        query=f"""
+        select isi_rapat from rapat where id_pertandingan = \'{id_pertandingan}\'
+        """
+        cur.execute(query)
+        isi_rapat = cur.fetchall()
+
+        response.set_cookie("id_pertandingan", id_pertandingan)
+
+
+        context = {
+            'isi_rapat': isi_rapat
+        }
+
+        return render(request, 'isi_rapat.html', context)
+    if (request.method=="GET"):
+        id_manajer = request.COOKIES.get('id_pertandingan')
+        query=f"""
+        select isi_rapat from rapat where id_pertandingan = '{id_manajer}'
+        """
+        cur.execute(query)
+        isi_rapat = cur.fetchall()
+
+        context = {
+            'isi_rapat': isi_rapat
+        }
+
+        return render(request, 'isi_rapat.html', context)
+        
+
 
 def generate_random_receipt():
     random_numbers = [random.randint(1000, 9999) for _ in range(4)]
