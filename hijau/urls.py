eb1c5a9b9148c6334848ff952120d837bd685204
TehGaa <syosuasaputra1010@gmail.com>
2023-05-25 11:42:47 +0700

    feat: add penambahan pemain dan pelatih feature

--- hijau/urls.py
+++ hijau/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 app_name = "hijau"
 
 urlpatterns = [
-    # path('create-tim', create_tim, name='create_tim'),
-    # path('show-tim', show_tim, name='show_tim'),
+    path('', home, name="home"),
+    path('create-tim', create_tim, name='create_tim'),
+    path('update-captain', update_captain, name='update-captain'),
+    path('delete-pemain', delete_pemain, name='delete-pemain'),
+    path('add-pemain', add_pemain, name='add-pemain'),
+    
 ]
