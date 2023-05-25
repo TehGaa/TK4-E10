6e628c9ddf944987ff862392e9048c01ef9f284a
reyvannn <reyvannn@gmail.com>
2023-05-25 23:29:00 +0700

    pink

--- pink/urls.py
+++ pink/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from pink.views import index, pinjam_stadium, pinjam_stadium_form
+from pink.views import index, pinjam_stadium, pinjam_stadium_form, tambah_peminjaman_stadium, mulai_rapat, isi_rapat, \
+    process_isi_rapat
 
 app_name = 'pink'
 
@@ -7,4 +8,8 @@ urlpatterns = [
     path('', index, name='index'),
     path('/pinjam_stadium/', pinjam_stadium, name='pinjam_stadium'),
     path('/pinjam_stadium/form/', pinjam_stadium_form, name='pinjam_stadium_form'),
+    path('/tambah_peminjaman_stadium/', tambah_peminjaman_stadium, name='tambah_peminjaman_stadium'),
+    path('/mulai_rapat/<uuid:pertandingan_id>/', isi_rapat, name='mulai_rapat_id'),
+    path('/mulai_rapat/', mulai_rapat, name='mulai_rapat'),
+    path('/process_isi_rapat/', process_isi_rapat, name='process_isi_rapat'),
 ]
--- pink/views.py
+++ pink/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 import psycopg2
 import psycopg2.extras
 import json
-from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest, HttpResponseRedirect, JsonResponse, QueryDict
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
 import uuid
+
+from putih.decorators.logged_in_decorators import login_required, login_required_as_role
 
 # Create your views here.
 conn = psycopg2.connect(database=settings.DATABASE_NAME,
@@ -21,20 +23,114 @@ cur = conn.cursor()
 def index(request):
     return render(request, 'index.html')
 
+@login_required
+@login_required_as_role('manajer')
 def pinjam_stadium(request):
-    query = f"SELECT * FROM peminjaman"
+    query = f"SELECT nama, start_datetime, end_datetime FROM peminjaman, stadium WHERE peminjaman.id_stadium = stadium.id_stadium;"
     cur.execute(query)
     res = cur.fetchall()
     context = {'peminjaman_list': res}
     return render(request, 'peminjaman-stadium.html', context)
 
+@login_required
+@login_required_as_role('manajer')
 def pinjam_stadium_form(request):
-    query = "SELECT nama FROM stadium"
+    query = "SELECT * FROM stadium"
     cur.execute(query)
     res = cur.fetchall()
-    stadiums = [item[0] for item in res]
-    return render(request, 'peminjaman-stadium-form.html', {'stadiums': stadiums})
+    context = {'stadium_list' : res}
+    return render(request, 'peminjaman-stadium-form.html', context)
 
-def tambah_peminjaman_stadium():
+@login_required
+@login_required_as_role('manajer')
+def tambah_peminjaman_stadium(request):
+    id_manajer = request.COOKIES.get("id_role")
+    stadium = request.POST['stadium']
+    start_datetime = request.POST['start_date']
+    end_datetime = request.POST['end_date']
+    print(request.POST)
+    try:
+        cur.execute(
+            "INSERT INTO peminjaman (id_manajer, start_datetime, end_datetime, id_stadium) VALUES (%s, %s, %s, %s);",
+            [id_manajer, start_datetime, end_datetime, stadium])
+        conn.commit()
+        return HttpResponse("Berhasil")
+    except Exception as e:
+        conn.rollback()
+        return HttpResponseBadRequest(e)
+    return render(request, 'index.html')
 
-    return
+@login_required
+@login_required_as_role('panitia')
+def mulai_rapat(request):
+    query = """
+            SELECT DISTINCT ON (B.id_pertandingan) B.id_pertandingan, A.nama_tim AS nama_tim_a, B.nama_tim AS nama_tim_b, P.start_datetime, P.end_datetime, S.nama AS nama_stadium
+            FROM tim_pertandingan AS A
+            JOIN tim_pertandingan AS B ON A.id_pertandingan = B.id_pertandingan AND A.nama_tim != B.nama_tim
+            JOIN pertandingan AS P ON A.id_pertandingan = P.id_pertandingan
+            JOIN stadium AS S ON P.stadium = S.id_stadium
+            WHERE NOT EXISTS (
+                SELECT 1
+                FROM rapat
+                WHERE rapat.id_pertandingan = A.id_pertandingan
+            );
+                """
+    cur.execute(query)
+    data = cur.fetchall()
+    context = {'pertandingan_list': data}
+    return render(request, 'mulai-rapat.html', context)
+
+@login_required
+@login_required_as_role('panitia')
+def isi_rapat(request, pertandingan_id):
+    query = """
+        SELECT DISTINCT ON (B.id_pertandingan) B.id_pertandingan, A.nama_tim AS nama_tim_a, B.nama_tim AS nama_tim_b
+        FROM tim_pertandingan AS A, tim_pertandingan AS B
+        WHERE A.id_pertandingan = B.id_pertandingan AND A.nama_tim != B.nama_tim AND A.id_pertandingan = %s;
+    """
+
+    cur.execute(query, [str(pertandingan_id)])
+    data = cur.fetchone()
+    return render(request, 'isi-rapat.html', {'pertandingan': data})
+
+@login_required
+@login_required_as_role('panitia')
+def process_isi_rapat(request):
+    id_pertandingan = request.POST.get('pertandinganId')
+    hasil_rapat = request.POST.get('isiRapat', '')
+    perwakilan_panitia = request.COOKIES.get("id_role")
+    cur.execute("""
+            SELECT DISTINCT ON (B.id_pertandingan)
+    B.id_pertandingan,
+    A.nama_tim AS nama_tim_a,
+    B.nama_tim AS nama_tim_b,
+    MA.id_manajer AS id_manajer_a,
+    MB.id_manajer AS id_manajer_b,
+    P.start_datetime
+FROM
+    tim_pertandingan AS A
+    JOIN tim_pertandingan AS B ON A.id_pertandingan = B.id_pertandingan AND A.nama_tim != B.nama_tim
+    JOIN tim_manajer AS MA ON A.nama_tim = MA.nama_tim
+    JOIN tim_manajer AS MB ON B.nama_tim = MB.nama_tim
+    JOIN pertandingan AS P ON A.id_pertandingan = P.id_pertandingan
+WHERE A.id_pertandingan=%s;
+        """, [str(id_pertandingan)])
+    fetch = cur.fetchone()
+    data = {'data': fetch}
+    print(data)
+    manajer_tim_a = data.get('data')[3]
+    manajer_tim_b = data.get('data')[4]
+    datetime = data.get('data')[5]
+    query ="""
+    INSERT INTO rapat (id_pertandingan, datetime, perwakilan_panitia, manajer_tim_a, manajer_tim_b, isi_rapat)
+VALUES (%s, %s, %s, %s, %s, %s);
+    """
+
+    try:
+        cur.execute(query, [id_pertandingan, datetime, perwakilan_panitia, manajer_tim_a, manajer_tim_b, hasil_rapat])
+        conn.commit()
+        return HttpResponseRedirect(reverse('pink:mulai_rapat'))
+    except Exception as e:
+        conn.rollback()
+        return HttpResponseBadRequest(e)
+    return HttpResponseRedirect(reverse('pink:mulai_rapat'))
